09899b44cc4dbf1a3d0c06b4589db87f302ae3ad
Derek Hall <Hderek22@icloud.com>
2022-07-18 12:39:49 -0400

    fixed typos

--- intro/projects.py
+++ intro/projects.py
@@ -9,10 +9,10 @@ def write():
 
 **Machine Learning Crypto Trading Algorithm/Bot[(Code)](https://github.com/Hderek22/machine_learning_crypto_algo.git)**
 - Built a logistic regression model using Sklearn
-- Connected to Alpaca Finance API's with trading signals based on market movement.
+- Connected to Alpaca Finance API's with trading signals based on market movement
 
 **Blockchain Payment Processing Tool[(Code)](https://github.com/Hderek22/blockchain_payment_processing_tool.git)**
-- Created a blockchain currency and built a Streamlit app for making payments with the new currency.
+- Created a blockchain currency and built a Streamlit app for making payments with the new currency
 
 **Crowdsale KaseiCoin[(Code)](https://github.com/Hderek22/CrowdsaleKaseiCoin.git)**
 - Created ab ERC20 token - KasaiCoin
